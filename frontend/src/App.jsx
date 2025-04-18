37f4964d921220eb13919003056b74c6c8122e4f
Maicol Osorio <osoriomaicol793@gmail.com>
2025-04-17 19:35:22 -0500

    Added category pages with dynamic banners and filtering. Created ShopContext for product state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
 
+import bannermens from "./assets/bannermens.png"
+import bannerwomens from "./assets/bannerwomens.png"
+import bannerkids from "./assets/bannerkids.png"
+
 export default function App() {
   return (
     <main className="bg-primary text-tertiary">
@@ -14,9 +18,9 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/mens" element={<Category />} />
-          <Route path="/womens" element={<Category />} />
-          <Route path="/kids" element={<Category />} />
+          <Route path="/mens" element={<Category banner={bannermens} category={"men"} />}/>
+          <Route path="/womens" element={<Category banner={bannerwomens} category={"women"}/>} />
+          <Route path="/kids" element={<Category banner={bannerkids} category={"kid"}/>}/>
           <Route path="/product" element={Products}>
             <Route path=":productId" element={Products}></Route>
           </Route>
--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,8 +1,32 @@
-function Category() {
+import { MdOutlineKeyboardArrowDown } from "react-icons/md"
+import all_products from "../assets/all_products.js"
+import Item from "../components/Item.jsx"
+function Category({ category, banner }) {
   return (
-    <div>
-        <h1>category</h1>
-    </div>
+    <section className="max_padd_container py-12 xl:py-28">
+      <div>
+        <div>
+          <img src={banner} alt={category} className="block my-7 mx-auto" />
+        </div>
+        <div className="flexBetween my-8 mx-2">
+          <h5 className="font-bold">showing 1-12<span>out of 36 products</span></h5>
+          <div className="flexBetween mx-sm:p-4 gap-x-4 px-8 py-3 rounded-5xl ring-1 ring-slate-800/15">sort by<MdOutlineKeyboardArrowDown /></div>
+        </div>
+        {/* container */}
+        <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        {all_products.map((item) => {
+          if (item.category == category) {
+            return (
+              <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            )
+          }
+        })}
+        </div>
+        <div className="mt-16 text-center">
+          <button className="btn_dark_rounded">Load more</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
